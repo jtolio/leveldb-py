df9ba63735055e302608d11dd3207162aafd25e3
JT Olds <hello@jtolds.com>
2012-07-25 14:07:04 -0600

    unittests for scoped db

--- test_leveldb.py
+++ test_leveldb.py
@@ -121,6 +121,83 @@ class LevelDBTestCases(unittest.TestCase):
         db.close()
 
 
+class ScopedDBTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_path = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.db_path, ignore_errors=True)
+
+    def testInclusive(self, use_writebatch=False):
+        db = leveldb.DB(self.db_path, create_if_missing=True)
+        scoped_db_1 = db.scope("prefix1_")
+        scoped_db_2 = db.scope("prefix2_")
+        scoped_db_2a = scoped_db_2.scope("a_")
+        scoped_db_2b = scoped_db_2.scope("b_")
+        scoped_db_3 = db.scope("prefix3_")
+
+        def mod(op, db, ops):
+            if use_writebatch:
+                batch = leveldb.WriteBatch()
+                for args in ops:
+                    getattr(batch, op)(*args)
+                db.write(batch)
+            else:
+                for args in ops:
+                    getattr(db, op)(*args)
+
+        mod("put", db, [("1", "2"), ("prefix2_a_13", "14")])
+        mod("put", scoped_db_1, [("3", "4")])
+        mod("put", scoped_db_2, [("5", "6")])
+        mod("put", scoped_db_2a, [("7", "8")])
+        mod("put", scoped_db_2b, [("9", "10")])
+        mod("put", scoped_db_3, [("11", "12")])
+        db_data = [("1", "2"), ("prefix1_3", "4"), ("prefix2_5", "6"),
+                   ("prefix2_a_13", "14"), ("prefix2_a_7", "8"),
+                   ("prefix2_b_9", "10"), ("prefix3_11", "12")]
+        self.assertEquals(list(db), db_data)
+        self.assertEquals(list(scoped_db_1), [("3", "4")])
+        scoped_db_2_data = [("5", "6"), ("a_13", "14"), ("a_7", "8"),
+                ("b_9", "10")]
+        self.assertEquals(list(scoped_db_2), scoped_db_2_data)
+        self.assertEquals(list(scoped_db_2a), [("13", "14"), ("7", "8")])
+        self.assertEquals(list(scoped_db_2b), [("9", "10")])
+        self.assertEquals(list(scoped_db_3), [("11", "12")])
+        for key, val in db_data:
+            self.assertEquals(db.get(key), val)
+        for key, val in scoped_db_2_data:
+            self.assertEquals(scoped_db_2.get(key), val)
+        self.assertEquals(scoped_db_1.get("3"), "4")
+        self.assertEquals(scoped_db_2a.get("7"), "8")
+        self.assertEquals(scoped_db_2b.get("9"), "10")
+        self.assertEquals(scoped_db_3.get("11"), "12")
+        self.assertEqual(scoped_db_2a.get("13"), "14")
+        mod("delete", db, [["1"], ["prefix2_a_7"]])
+        mod("delete", scoped_db_1, [["3"]])
+        mod("delete", scoped_db_2, [["5"]])
+        mod("delete", scoped_db_2a, [["13"]])
+        mod("delete", scoped_db_2b, [["9"]])
+        mod("delete", scoped_db_3, [["11"]])
+        self.assertEquals(list(db), [])
+        self.assertEquals(list(scoped_db_1), [])
+        self.assertEquals(list(scoped_db_2), [])
+        self.assertEquals(list(scoped_db_2a), [])
+        self.assertEquals(list(scoped_db_2b), [])
+        self.assertEquals(list(scoped_db_3), [])
+        for key, val in db_data:
+            self.assertEquals(db.get(key), None)
+        for key, val in scoped_db_2_data:
+            self.assertEquals(scoped_db_2.get(key), None)
+        self.assertEquals(scoped_db_1.get("3"), None)
+        self.assertEquals(scoped_db_2a.get("7"), None)
+        self.assertEquals(scoped_db_2b.get("9"), None)
+        self.assertEquals(scoped_db_3.get("11"), None)
+        self.assertEqual(scoped_db_2a.get("13"), None)
+
+    def testInclusive_WriteBatch(self):
+        self.testInclusive(use_writebatch=True)
+
 def main():
     parser = argparse.ArgumentParser("run tests")
     parser.add_argument("--runs", type=int, default=1)
