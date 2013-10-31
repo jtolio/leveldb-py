ca518c6c422f44c505803865dd2c01d93d0d5663
JT Olds <hello@jtolds.com>
2013-10-31 15:36:06 -0600

    packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name='leveldbpy',
+    version='0.1',
+    description='Python bindings for LevelDB',
+    author='JT Olds',
+    author_email='jt@spacemonkey.com',
+    url="http://leveldb-py.googlecode.com/",
+    py_modules = ["leveldb"])
